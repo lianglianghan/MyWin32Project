dd9d1d6d7cb9369c3b0568c99bd1ee5738baa937
liangliang <qiuhunjihl@163.com>
2013-10-19 16:03:53 +0800

    final test

---
+++ Timer&Mouse/resource_2.h
@@ -0,0 +1,37 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by 201310151712.rc
+//
+#define IDR_MENU1                       101
+#define IDR_ACCELERATOR1                102
+#define IDR_ACCELERATOR2                104
+#define IDD_DIALOG1                     105
+#define IDD_DIALOG2                     106
+#define IDD_DIALOG3                     107
+#define IDD_DIALOG4                     109
+#define IDC_EDIT1                       1001
+#define IDC_BUTTON1                     1002
+#define IDC_STATIC1                     1003
+#define IDC_STATIC2                     1004
+#define ID_40001                        40001
+#define ID_40002                        40002
+#define ID_40003                        40003
+#define ID_40004                        40004
+#define ID_ACCELERATOR40007             40007
+#define ID_ACCELERATOR40008             40008
+#define ID_ACCELERATOR40009             40009
+#define ID_ACCELERATOR40010             40010
+#define ID_ACCELERATOR40011             40011
+#define ID_ACCELERATOR40012             40012
+#define ID_40014                        40014
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_COMMAND_VALUE         40015
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
